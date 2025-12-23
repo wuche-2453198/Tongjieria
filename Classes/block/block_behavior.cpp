7608504e8a8eb4351b8f3f36b1962e4a74780d65
TheBBPigeon <3422289296@qq.com>
2025-12-23 16:12:12 +0800

    实现了方块的基础组件，包括区块，区块的加载和物理互动等

---
+++ Classes/block/block_behavior.cpp
@@ -0,0 +1,45 @@
+#include "block_behavior.h"
+
+BlockBehaviorRegistry::BlockBehaviorRegistry()
+{
+    _behaviors[entt::hashed_string("dirt")] = std::make_shared<DirtBehavior>();
+}
+
+BlockBehaviorRegistry::~BlockBehaviorRegistry() = default;
+
+const std::shared_ptr<BlockBehavior> const BlockBehaviorRegistry::getBehavior(entt::id_type id) const
+{
+    if(_behaviors.find(id) != _behaviors.end()) 
+    {
+        return _behaviors.at(id);
+    }
+    else
+    {
+        return nullptr;
+    }
+}
+
+void DirtBehavior::onBlockPlaced(const BlockPlacedEvent& event)
+{
+
+}
+
+void DirtBehavior::onBlockDestroyed(const BlockDestroyEvent& event)
+{
+}
+
+void DirtBehavior::onBlockMined(const BlockMinedEvent& event)
+{
+}
+
+void DirtBehavior::onBlockNeighborChanged()
+{
+}
+
+void DirtBehavior::onBlockInteracted(const BlockInteractEvent& event)
+{
+}
+
+void DirtBehavior::onRamdomTick()
+{
+}
