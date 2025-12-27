c09db99ae1d56f28d972999f79b13761947f2312
TheBBPigeon <3422289296@qq.com>
2025-12-27 15:42:26 +0800

    修改了部分类名称

--- Classes/systems/block/block_mining_system.cpp
+++ Classes/systems/block/block_mining_system.cpp
@@ -15,79 +15,47 @@ void BlockMiningSystem::update(float delta)
 
 void BlockMiningSystem::mining(float delta)
 {
-    auto view = _registry.view<ActiveBlock, MiningTag>();
-    view.each([&](entt::entity entity, ActiveBlock& block, MiningTag& tag)
+    auto view = _registry.view<BlockEntityHead, MiningTag>();
+    view.each([&](entt::entity entity, BlockEntityHead& blockEntity, MiningTag& tag)
         {
             auto progress = _registry.try_get<MiningProgress>(entity);
             if (!progress)
             {
-                progress = &(block.saveEmplace<MiningProgress>(_registry, entity));
+                progress = &(blockEntity.saveEmplace<MiningProgress>(_registry, entity));
             }
             // ∆∆ªµ∑ΩøÈ
             progress->progress += tag.factor * delta;
-
-            CCLOG("Mined at %d %d, progress= %f",
-                block.blockPos.x, block.blockPos.y,
-                progress->progress);
 
             // Õ⁄æÚÕÍ≥…£¨≥¢ ‘∆∆ªµ
             if (progress->progress > 1.0f)
             {
                 auto& blockWorld = _registry.ctx().get<BlockWorld>();
-                if (blockWorld.tryDestroy(block.blockPos, tag.minier))
+                if (blockWorld.tryDestroy(blockEntity.layer , blockEntity.blockPos, tag.miner))
                 {
-                    block.saveRemove<MiningProgress>(_registry, entity);
+                    blockEntity.saveRemove<MiningProgress>(_registry, entity);
                 }
             }
-            block.saveRemove<MiningTag>(_registry, entity);
+            blockEntity.saveRemove<MiningTag>(_registry, entity);
         });
 }
 
 void BlockMiningSystem::render()
 {   
     // ‘ˆº”‰÷»æ÷∏¡Ó
-    auto needToAdd = _registry.view<ActiveBlock, MiningProgress>(entt::exclude<CustomcommandPack>);
-    needToAdd.each([&](entt::entity entity, ActiveBlock& block, MiningProgress& progress)
+    auto needToAdd = _registry.view<BlockEntityHead, MiningProgress>(entt::exclude<CustomcommandPack>);
+    needToAdd.each([&](entt::entity entity, BlockEntityHead& block, MiningProgress& progress)
         {
-            auto texture = _registry.ctx().get<AssetManager>().getTexture("blocks\\textures\\stone.png");
-            auto command = new BlockBatchCommand(1, block.blockPos, texture);
+            auto texture = _registry.ctx().get<AssetManager>().getTexture("blocks\\textures\\mining2.png");
+            auto command = new BlockBatchCommand(10, block.blockPos, texture);
             command->setUseTransform(false);
             auto& pack = block.saveEmplace<CustomcommandPack>(_registry, entity);
             pack.commands.push_back(command);
         });
 
-    // À¢–¬‰÷»æ÷∏¡Ó
-    auto needToUpdate = _registry.view<ActiveBlock, MiningProgress, CustomcommandPack>();
-    needToUpdate.each([&](entt::entity entity, ActiveBlock& block, MiningProgress& progress, CustomcommandPack& pack)
-        {
-            pack.releaseAllCommand();
-            auto command = new BlockBatchCommand(1, block.blockPos, getProgressTexture(progress.progress));
-            command->setUseTransform(false);
-            pack.commands.push_back(command);
-        });
-
     // “∆≥˝Œﬁ–ßµƒ‰÷»æ÷∏¡Ó
-    auto needToRemove = _registry.view<ActiveBlock, CustomcommandPack>(entt::exclude<MiningProgress>);
-    needToRemove.each([&](entt::entity entity, ActiveBlock& block, CustomcommandPack& pack)
+    auto needToRemove = _registry.view<BlockEntityHead, CustomcommandPack>(entt::exclude<MiningProgress>);
+    needToRemove.each([&](entt::entity entity, BlockEntityHead& block, CustomcommandPack& pack)
         {
             block.saveRemove<CustomcommandPack>(_registry, entity);
         });
 }
-
-cocos2d::Texture2D* BlockMiningSystem::getProgressTexture(float progress)
-{
-    if (progress <= 0)
-    {
-        return _assetManager.getTexture("blocks\\textures\\air.png");
-    }
-    if (progress <= 0.33f)
-    {
-        return _assetManager.getTexture("blocks\\textures\\mining1.png");
-    }
-    if (progress <= 0.66f)
-    {
-        return _assetManager.getTexture("blocks\\textures\\mining2.png");
-    }
-    return _assetManager.getTexture("blocks\\textures\\mining3.png");
-}
-
