1ec80de22e7487544e41eec6fc494bce458bcc53
TheBBPigeon <3422289296@qq.com>
2025-12-27 15:44:16 +0800

    增加了新系统BlockGenSystem和BlockEntityCleanSystem的初始化代码

--- Classes/systems/block/block_system_manager.cpp
+++ Classes/systems/block/block_system_manager.cpp
@@ -1,14 +1,19 @@
+#include "core/block_world.h"
 #include "chunk_load_system.h"
 #include "block_load_system.h"
+#include "block_gen_system.h"
 #include "chunk_unload_system.h"
 #include "block_mining_system.h"
+#include "block_entity_clean_system.h"
 #include "block_physics_system.h"
 #include "chunk_render_system.h"
 #include "block_interact_system.h"
 #include "debug_system.h" 
 
 ISystem::ISystem(entt::registry& registry, entt::dispatcher& dispatcher)
-    : _registry(registry), _dispatcher(dispatcher) {}
+    : _registry(registry), 
+    _dispatcher(dispatcher), 
+    _blockWorld(_registry.ctx().get<BlockWorld>()) {}
 
 ISystem::~ISystem() = default;
 
@@ -22,8 +27,10 @@ BlockSystemManager::BlockSystemManager(entt::registry& registry, entt::dispatche
     // 初始化各个子系统
     _chunkLoadSystem = std::make_unique<ChunkLoadSystem>(_registry, _dispatcher);
     _blockLoadSystem = std::make_unique<BlockLoadSystem>(_registry, _dispatcher);
+    _blockGenSystem = std::make_unique<BlockGenSystem>(_registry, _dispatcher);
     _chunkUnloadSystem = std::make_unique<ChunkUnloadSystem>(_registry, _dispatcher);
     _blockMiningSystem = std::make_unique<BlockMiningSystem>(_registry, _dispatcher);
+    _blockEntityCleanSystem = std::make_unique<BlockEntityCleanSystem>(_registry, _dispatcher);
     _blockPhysicsSystem = std::make_unique<BlockPhysicsSystem>(_registry, _dispatcher);
     _chunkRenderCommandSystem = std::make_unique<ChunkRenderSystem>(_registry, _dispatcher);
     _blockInteractSystem = std::make_unique<BlockInteractSystem>(_registry, _dispatcher);
@@ -36,8 +43,10 @@ void BlockSystemManager::update(float delta)
     // 按顺序更新各个子系统
     _chunkLoadSystem->update(delta);
     _blockLoadSystem->update(delta);
+    _blockGenSystem->update(delta);
     _chunkUnloadSystem->update(delta);
     _blockMiningSystem->update(delta);
+    _blockEntityCleanSystem->update(delta);
     _blockPhysicsSystem->update(delta);
     _chunkRenderCommandSystem->update(delta);
     _debugSystem->update(delta);
--- Classes/systems/block/block_system_manager.h
+++ Classes/systems/block/block_system_manager.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "entt/entt.hpp"
 
+class BlockWorld;
+
 class ISystem
 {
 public:
@@ -10,14 +12,17 @@ public:
 protected:
     entt::registry& _registry;
     entt::dispatcher& _dispatcher;
+    BlockWorld& _blockWorld;
 };
 
 class Vec2i;
 class BlockLayer;
 class ChunkLoadSystem;
 class BlockLoadSystem;
+class BlockGenSystem;
 class ChunkUnloadSystem;
 class BlockMiningSystem;
+class BlockEntityCleanSystem;
 class BlockPhysicsSystem;
 class ChunkRenderSystem;
 class BlockInteractSystem;
@@ -34,8 +39,10 @@ private:
 
     std::unique_ptr<ChunkLoadSystem> _chunkLoadSystem = nullptr;
     std::unique_ptr<BlockLoadSystem> _blockLoadSystem = nullptr;
+    std::unique_ptr<BlockGenSystem> _blockGenSystem = nullptr;
     std::unique_ptr<ChunkUnloadSystem> _chunkUnloadSystem = nullptr;
     std::unique_ptr<BlockMiningSystem> _blockMiningSystem = nullptr;
+    std::unique_ptr<BlockEntityCleanSystem> _blockEntityCleanSystem = nullptr;
     std::unique_ptr<BlockPhysicsSystem> _blockPhysicsSystem = nullptr;
     std::unique_ptr<ChunkRenderSystem> _chunkRenderCommandSystem = nullptr;
     std::unique_ptr<BlockInteractSystem> _blockInteractSystem = nullptr;
